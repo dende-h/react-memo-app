3a3e764d9f6b95b42033bc11df2d460f1ff22d29
dende-h <miyuki_dende@yahoo.co.jp>
2022-01-14 00:30:38 +0900

    フッターの外部リンク設置

--- src/components/organism/TitleFooter.tsx
+++ src/components/organism/TitleFooter.tsx
@@ -1,6 +1,9 @@
 import { Link, HStack, Center, Box, Stack } from "@chakra-ui/react";
 
 export const TitleFooter = () => {
+	const onClickLink = (url: string) => {
+		window.location.assign(url);
+	};
 	return (
 		<>
 			<Center bg={"teal.100"} h={"200px"} paddingBottom={10}>
@@ -14,11 +17,17 @@ export const TitleFooter = () => {
 						spacing={["20px", "30px", "100px"]}
 						textAlign={"center"}
 					>
-						<Link>{`DenDe's Github page`}</Link>
-						<Link>Twitter</Link>
-						<Link>Qiita My page</Link>
-						<Link>{`Development memo's`}</Link>
-						<Link>{`BuildUp portfolio`}</Link>
+						<Link onClick={() => onClickLink("https://github.com/dende-h")}>{`DenDe's Github page`}</Link>
+						<Link onClick={() => onClickLink("https://twitter.com/dende49592814")}>Twitter</Link>
+						<Link onClick={() => onClickLink("https://qiita.com/dende-h")}>Qiita My page</Link>
+						<Link
+							onClick={() =>
+								onClickLink("https://perpetual-hemisphere-7a3.notion.site/4d56af5f032b484186cef530f8d77628")
+							}
+						>{`Development memo's`}</Link>
+						<Link
+							onClick={() => onClickLink("https://app.build-up.info/portfolio/b5UTNgoTd")}
+						>{`BuildUp portfolio`}</Link>
 					</HStack>
 				</Stack>
 			</Center>
--- src/hooks/useRouteHandle.tsx
+++ src/hooks/useRouteHandle.tsx
@@ -5,17 +5,11 @@ type ClickRouter = (urlProps: string, state?: object, replace?: boolean) => void
 
 export const useRouteHandle = () => {
 	const navigate: NavigateFunction = useNavigate();
-	const onDoubleClickRouter: ClickRouter = useCallback(
-		(urlProps, state, replace) => {
-			navigate(urlProps, { state, replace });
-		},
-		[navigate]
-	);
-	const onClickRouter: ClickRouter = useCallback(
-		(urlProps, state, replace) => {
-			navigate(urlProps, { state, replace });
-		},
-		[navigate]
-	);
+	const onDoubleClickRouter: ClickRouter = useCallback((urlProps, state, replace) => {
+		navigate(urlProps, { state, replace });
+	}, []);
+	const onClickRouter: ClickRouter = useCallback((urlProps, state, replace) => {
+		navigate(urlProps, { state, replace });
+	}, []);
 	return { onDoubleClickRouter, onClickRouter };
 };
