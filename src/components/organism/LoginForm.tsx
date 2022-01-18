3f5eaf206d5a4346a667814aa5cf3d4f27b16451
dende-h <miyuki_dende@yahoo.co.jp>
2022-01-18 10:50:50 +0900

    useAuth作成、useInputForm作成

--- src/components/organism/LoginForm.tsx
+++ src/components/organism/LoginForm.tsx
@@ -1,41 +1,19 @@
 import { Box, Button, Divider, Flex, Heading, Input, Spinner, Stack } from "@chakra-ui/react";
-import { AxiosResponse } from "axios";
-import { memo, useState, VFC } from "react";
-import toast from "react-hot-toast";
-import { NavigateFunction, useNavigate } from "react-router-dom";
-import { loginApi } from "../../env/api";
+import { memo, VFC } from "react";
+import { useAuthLogin } from "../../hooks/useAuthLogin";
+import { useInputForm } from "../../hooks/useInputForm";
 import { PasswordInput } from "../molecule/PasswordInput";
 
 export const LoginForm: VFC = memo(() => {
-	const [email, setEmail] = useState<string>("");
-	const [password, setPassword] = useState<string>("");
-	const [loading, setLoading] = useState<boolean>(false);
+	const email = useInputForm();
+	const password = useInputForm();
 
-	const authKey = {
-		email,
-		password
-	};
+	const authKey = { email: email.value, password: password.value };
 
-	const navigate: NavigateFunction = useNavigate();
-	const onClickLoginButton: React.MouseEventHandler<HTMLButtonElement> = async () => {
-		try {
-			setLoading(true);
-			const result: AxiosResponse = await loginApi.post("/login", authKey);
-			localStorage.setItem("authToken", result.data.access_token);
-			console.log(result);
-			navigate("/top", { state: result, replace: false });
-		} catch (error) {
-			setLoading(false);
-			console.log(authKey);
-			toast.error("ログインできません!");
-		}
-	};
-	const onChangeInputEmail: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-		setEmail(e.target.value);
-	};
-	const onChangeInputPassword: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-		setPassword(e.target.value);
-	};
+	const { authLogin, loading } = useAuthLogin();
+
+	const onClickLoginButton: React.MouseEventHandler<HTMLButtonElement> = () => authLogin(authKey);
+
 	return (
 		<>
 			<Flex justify="center" minHeight="">
@@ -45,12 +23,18 @@ export const LoginForm: VFC = memo(() => {
 					</Heading>
 					<Divider my={4} />
 					<Stack spacing={6} py={4} px={10}>
-						<Input bg="grey.200" placeholder="Enter email" type={"email"} value={email} onChange={onChangeInputEmail} />
+						<Input
+							bg="grey.200"
+							placeholder="Enter email"
+							type={"email"}
+							value={email.value}
+							onChange={email.onChangeInputForm}
+						/>
 						<PasswordInput
 							bg="grey.200"
 							placeholder="Enter Password"
-							value={password}
-							onChange={onChangeInputPassword}
+							value={password.value}
+							onChange={password.onChangeInputForm}
 						/>
 						<Button colorScheme={"twitter"} outline="none" onClick={onClickLoginButton}>
 							{loading ? (
